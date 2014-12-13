bb68d1f8dabe9c382e1876a69017c136741bd36a
少杰 <sslikewater@gmail.com>
2014-12-13 10:18:23 +0800

    use touch event on mobile devices

--- static/js/event.js
+++ static/js/event.js
@@ -1,5 +1,5 @@
 var current_switch_state = true;
-var is_mobile = (navigator.userAgent.indexof("Android")!=-1 || navigator.userAgent.indexof("iPhone")!=-1)
+var is_mobile = (navigator.userAgent.Indexof("Android")!=-1 || navigator.userAgent.Indexof("iPhone")!=-1)
 
 function set_click_handler (element, handler) {
 	if (is_mobile) {
