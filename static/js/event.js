25e6901d8239edbde84974c7e247f56072a4caab
李少杰 <super@joker.li>
2017-07-03 00:00:33 +0800

    add new ui

--- static/js/event.js
+++ static/js/event.js
@@ -1,67 +1,135 @@
-var current_switch_state = 0;
-
-function is_state_on (state) {
-	return state == 1 || state == '1' || state == true;
+function is_state_on(state) {
+    return state == 1 || state == '1' || state == true;
 }
 
-var is_mobile = (navigator.userAgent.indexOf("Android")!=-1 || navigator.userAgent.indexOf("iPhone")!=-1)
-
-function set_click_handler (element, handler) {
-	if (is_mobile) {
-		console.log('device is mobile')
-		element.ontouchend = handler;
-	} else {
-		console.log('device is desktop computer')
-		element.onclick = handler;
-	}
+function ajax(path, method, async, listener) {
+    var xhr = new XMLHttpRequest();
+    var host = "http://home.joker.li:3568"
+    xhr.open(method, host + path, async);
+    xhr.onreadystatechange = function(e) {
+        if (!listener) {
+            return;
+        }
+        var xhr = e.target;
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200 && listener.success) {
+                listener.success(xhr);
+            } else if (listener.error) {
+                listener.error(xhr);
+            }
+        }
+    };
+    xhr.send();
 }
 
-function ajax (path, method, async, listener) {
-	var xhr = new XMLHttpRequest();
-	xhr.open(method, path, async);
-	xhr.onreadystatechange = function () {
-		listener(xhr);
-	};
-	xhr.send();
+function simple_get(path, listener) {
+    ajax(path, "GET", true, listener);
 }
 
-function simple_get (path, listener) {
-	ajax(path, "GET", true, listener);
+function get_lights(listener) {
+    simple_get("/lights", {
+        success: function(xhr) {
+            var res = JSON.parse(xhr.response);
+            listener(res);
+        }
+    })
 }
 
-function switch_light_ui (light_on) {
-	document.getElementById('light-switch').setAttribute("on", light_on?"true":"false");
-	current_switch_state = light_on;
-	console.log('set light ui to '+light_on)
-}
+document.addEventListener("DOMContentLoaded", function() {
 
-function get_light_state (listener) {
-	simple_get("/light/state/get", function (xhr) {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			var state = parseInt(xhr.responseText);
-			console.log('got light state from server: '+state)
-			listener(state);
-		};
-	})
-}
+    var scanVm = new Vue({
+        el: "#scan-dialog",
+        data: {
+        	scaning: true,
+            lights: []
+        },
+        methods: {
+            selectScan: function(e) {
+                var light = this.lights[e];
+                simple_get("/add/" + light.addr + "/" + light.name, {
+                    success: this.connectSuccess
+                });
+            },
+            connectSuccess: function(xhr) {
+                console.log(xhr.response);
+                get_lights(function(lights) {
+                    vm.ready = true;
+                    vm.lights = lights.map(function(lt) {
+                        if (lt.online) {
+                            lt.on = (lt.value === "N1");
+                        }
+                        return lt;
+                    });
+                    setTimeout(vm.updateMDL, 0);
+                });
+            },
+            scanBle: function() {
+                document.querySelector("#scan-dialog").showModal();
+                this.scaning = true;
+                simple_get("/scan", {
+                    success: this.onScanResult,
+                });
+            },
+            closeDialog: function() {
+                document.querySelector("#scan-dialog").close();
+            },
+            onScanResult: function(xhr) {
+                var bles = JSON.parse(xhr.response);
+                this.lights = bles;
+                this.scaning = false;
+            }
+        }
+    });
 
-function set_light_state (state, listener) {
-	simple_get("light/state/set?on="+(state), function (xhr) {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			console.log('send switch light command to server: '+state)
-			switch_light_ui(current_switch_state);
-		};
-	})
-}
+    var vm = new Vue({
+        el: "#smart-container",
+        data: {
+            ready: false,
+            showScanDialog: false,
+            lights: []
+        },
+        methods: {
+            updateMDL: function() {
+                document.querySelectorAll("label.mdl-switch").forEach(function(mdl_sw) {
+                    componentHandler.upgradeElement(mdl_sw);
+                })
+            },
+            addDevice: function(e) {
+                scanVm.scanBle();
+            },
+            updateName: function(e) {
+                var light = this.lights[e];
+                var name = prompt("给个新名字嘛~");
+                if (name) {
+                    simple_get("/light/name/" + light.addr + "/" + name);
+                }
+            },
+            toggleLightSwitch: function(e) {
+                var light = this.lights[e];
+                simple_get("/light/set/" + light.addr + "?val=S" + (light.on ? 0 : 1) + "$", {
+                    success: function(xhr) {
+                        if (xhr.response === "N1") {
+                            console.log("switch light on!");
+                        }
+                    },
+                    error: function(xhr) {
+                        console.error(JSON.stringify(xhr));
+                    }
+                });
+            }
+        }
+    });
 
-document.addEventListener("DOMContentLoaded", function () {
-	get_light_state(function (state) {
-		document.getElementById('start-overlay').style.display="none";
-		switch_light_ui(state);
-		set_click_handler(document.getElementById('light-switch'), function (event) {
-			current_switch_state = parseInt(current_switch_state)==0?1:0;
-			console.log('light-switch clicked, switch to state: '+current_switch_state)
-			set_light_state(current_switch_state);
-		});
-	})
+
+    get_lights(function(lights) {
+        vm.ready = true;
+        vm.lights = lights.map(function(lt) {
+            if (lt.online) {
+                lt.on = (lt.value === "N1");
+            }
+            return lt;
+        });
+        setTimeout(vm.updateMDL, 0);
+    });
+
 })
