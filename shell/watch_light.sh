ece063ff6fd32c73ccf3b910befce1b96b8b5d72
Shaojie Lee <sslikewater@gmail.com>
2017-07-13 22:53:34 +0800

    fix light watch

--- shell/watch_light.sh
+++ shell/watch_light.sh
@@ -5,9 +5,14 @@ night_light=29
 last_status=`gpio read $night_light`
 
 while [[ 1 ]]; do
-	if [[ $last_status == '0' && `gpio read $night_light` == '1' ]]; then
+	current=`gpio read $night_light`
+	if [[ $last_status == '0' && $current == '1' ]]; then
 		curl "http://home.joker.li:3568/light/set/00:15:85:10:79:54?val=S1$"
 	fi
+	if [[ $last_status == 1 && $current == 0 ]]; then
+		curl "http://home.joker.li:3568/light/set/00:15:85:10:79:54?val=S0$"
+	fi
+	last_status=$current
 	sleep 1
 done
 
